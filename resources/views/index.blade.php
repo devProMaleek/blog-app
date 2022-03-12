ca6f6bb8e9e7e67863c0250d255565f7dda65958
devProMaleek <milikiadbay@gmail.com>
2022-03-12 22:49:02 +0100

    feat: Create App and Authentication
    Create the project app, authenticartion working fine

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('homepage')
+<h1 class="text-center">Welcome to devProMaleek Blog App</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostsController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Auth::routes();
+
+Route::get('/', [PagesController::class, 'index'])->name('page.home');
+Route::resource('/blog', [PostsController::class]);
+
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
